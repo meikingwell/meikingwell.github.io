ac1af1bf32046f389da46dd1775899756477d539
meikingwell <54692836+meikingwell@users.noreply.github.com>
2019-11-14 19:34:18 +1100

    upload of projects

--- justkindadoingit/assets/js/main.js
+++ justkindadoingit/assets/js/main.js
@@ -6,4 +6,20 @@ $( document ).ready(function() {
     $( ".draggable" ).draggable();
   } );
 
+  $( function() {
+    $( ".draggable2" ).draggable();
+  } );
+
+  $( function() {
+    $( ".draggable3" ).draggable();
+  } );
+
+ $( function() {
+    $( ".draggable4" ).draggable();
+  } );
+
+ $( function() {
+    $( ".draggable5" ).draggable();
+  } );
+
 });
